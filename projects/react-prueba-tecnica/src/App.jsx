4968587fcdba44c7c8d86c3d26d2b8240aa6665c
Ricardo Miralles <ricmiber@gmail.com>
2023-03-16 12:48:24 +0100

    Adding third project

---
+++ projects/react-prueba-tecnica/src/App.jsx
@@ -0,0 +1,9 @@
+
+import { React, useState } from 'react'
+
+export default function App () {
+  const [fact, setFact] = useState('Esto es un hecho')
+  return (
+    <div>New Component </div>
+  )
+}
