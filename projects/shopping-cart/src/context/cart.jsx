6905a6a8b3738c8c72da61b986551c69905302b4
Ricardo Miralles <ricmiber@gmail.com>
2023-03-22 21:09:33 +0100

    Adding some features to reducer

--- projects/shopping-cart/src/context/cart.jsx
+++ projects/shopping-cart/src/context/cart.jsx
@@ -24,41 +24,10 @@ function useCartReducer () {
 
 // Creamos el provider
 export function CartProvider ({ children }) {
-  // const [cart, setCart] = useState([])
   const { state, addToCart, removeFromCart, clearCart } = useCartReducer()
 
-  // const addToCart = product => {
-  //   // Comprobar si el producto esta en el carrito
-  //   const isProductInCart = cart.some(item => item.id === product.id)
-  //   console.log(isProductInCart)
-
-  //   // Si el producto esta en el carrito sumamos 1 a la cantidad que tenia
-  //   if (isProductInCart) {
-  //     const productIndexInCart = cart.findIndex(item => item.id === product.id)
-  //     const newCart = structuredClone(cart)
-  //     newCart[productIndexInCart].quantity += 1
-  //     return setCart(newCart)
-  //   } else {
-  //     // Si no lo añadimos como un producto nuevo
-  //     setCart(prevState => ([
-  //       ...prevState,
-  //       {
-  //         ...product,
-  //         quantity: 1
-  //       }
-  //     ]))
-  //   }
-  // }
-
-  // const removeFromCart = (product) => {
-  //   return setCart(prevState => prevState.filter(item => item.id !== product.id))
-  // }
-  // const clearCart = () => {
-  //   setCart([])
-  // }
-
   return (
-    <CartContext.Provider value={{ cart: state, addToCart, removeFromCart, clearCart }}>
+    <CartContext.Provider value={{ cart: state.items, totalPay: state.totalPay, addToCart, removeFromCart, clearCart }}>
       {children}
     </CartContext.Provider>
   )
--- projects/shopping-cart/src/reducers/cart.js
+++ projects/shopping-cart/src/reducers/cart.js
@@ -1,4 +1,7 @@
-export const cartInitialState = JSON.parse(window.localStorage.getItem('cart')) || []
+export const cartInitialState = JSON.parse(window.localStorage.getItem('cart')) || {
+  items: [],
+  totalPay: 0
+}
 
 const updateLocalStorage = state => {
   window.localStorage.setItem('cart', JSON.stringify(state))
@@ -6,6 +9,7 @@ const updateLocalStorage = state => {
 
 export const cartReducer = (state, action) => {
   const { type, payload } = action
+  console.log('Payload', payload)
 
   const CART_ACTION_TYPES = {
     ADD_TO_CART: 'ADD_TO_CART',
@@ -13,31 +17,44 @@ export const cartReducer = (state, action) => {
     CLEAR_CART: 'CLEAR_CART'
   }
 
+  const products = state.items
+
   switch (type) {
     case CART_ACTION_TYPES.ADD_TO_CART: {
-      const isProductInCart = state.some(item => item.id === payload.id)
+      const isProductInCart = products.some(item => item.id === payload.id)
       if (isProductInCart) {
-        const productIndexInCart = state.findIndex(item => item.id === payload.id)
+        const productIndexInCart = products.findIndex(item => item.id === payload.id)
         const newState = structuredClone(state)
-        newState[productIndexInCart].quantity += 1
+        console.log(newState)
+        newState.items[productIndexInCart].quantity += 1
+        newState.totalPay = newState.totalPay + newState.items[productIndexInCart].price
         updateLocalStorage(newState)
         return newState
       } else {
         // Si no lo añadimos como un producto nuevo
-        const newState = [
+        const newItem = { ...payload, quantity: 1 }
+        const newState = {
           ...state,
-          {
-            ...payload,
-            quantity: 1
-          }
-        ]
+          items: [
+            ...state.items, newItem
+          ],
+          totalPay: state.totalPay + payload.price
+        }
         updateLocalStorage(newState)
         return newState
       }
     }
+
     case CART_ACTION_TYPES.REMOVE_FROM_CART: {
       const { id } = payload
-      const newState = state.filter(item => item.id !== id)
+      const itemToRemove = state.items.find(item => item.id === id)
+      const updateItems = state.items.filter(item => item.id !== id)
+
+      const newState = {
+        ...state,
+        items: updateItems,
+        totalPay: state.totalPay - itemToRemove.price * itemToRemove.quantity
+      }
       updateLocalStorage(newState)
       return newState
     }
